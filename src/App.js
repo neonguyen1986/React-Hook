64f2efa54ed99bbf843ca4e6a35e044641be8b5c
Neo Nguyen <neonguyen.ca@gmail.com>
2023-07-08 13:41:53 -0600

    4.temp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
 
 function App() {
+
+  const handleEventClick = (event) => {
+    console.log(">>>you've just click", event)
+  }
+
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
+        <h3>Hello world with React</h3>
+        {/* <input
+          type="text"
+          value='Minh'
+          onClick={(event) => handleEventClick(event)}
+        /> */}
+        <button
+          type="button"
+          onClick={(event) => handleEventClick(event)}
+        >
+          Click Me
+        </button>
       </header>
     </div>
   );
