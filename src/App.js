a589d902f3559e3f52eb0082f63b6707b52935ed
Neo Nguyen <neonguyen.ca@gmail.com>
2023-07-08 23:45:48 -0600

    5. State và useState với Hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
 
 function App() {
-
+  let [name, setName] = useState('Minh Nguyen')//trả về [a,b]
+  //hàm useState này sẽ trả về 1 array: 
+  //gồm name = a:giá trị của biến
+  //setName=b: một function xử lý khi biến này thay đổi
+  //cách gọi  const[a,b] được gọi là destructuring (giản lược cú pháp)
+  let [address, setAddress] = useState('Calgary')
   const handleEventClick = (event) => {
-    console.log(">>>you've just click51", event)
+    setName(address)
+    console.log(">>>you've just click", name)
+    console.log('---input content: ', address)
+  }
+
+  const handleOnChangeInput = (event) => {
+    setAddress(event.target.value)
   }
 
   return (
@@ -15,11 +25,11 @@ function App() {
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Hello world with React</h3>
+        <h3>Hello world with React with {name}</h3>
         <input
           type="text"
-          value='Minh'
-          onClick={(event) => handleEventClick(event)}
+          value={address}
+          onChange={(event) => handleOnChangeInput(event)}
         />
         <button
           type="button"
