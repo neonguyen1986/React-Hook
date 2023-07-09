666b1c7559896d6ba26d104b44c4e8fed6218871
Neo Nguyen <neonguyen.ca@gmail.com>
2023-07-09 01:03:05 -0600

    7.1 Props

---
+++ src/views/Todo.js
@@ -0,0 +1,17 @@
+const Todo = (props) => {
+    const todos = props.childTodos;
+    return (
+        <div className='todo-container'>
+            {todos && todos.length > 0 &&
+                todos.map((item, index) => {
+                    return (
+                        <li className="todo-child" key={item.id}>
+                            {index + 1} - {item.title}
+                        </li>
+                    )
+                })
+            }
+        </div>
+    )
+}
+export default Todo;
