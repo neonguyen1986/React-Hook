1a63cc83edbb83f31c9e30577db3f2a3001bf942
Neo Nguyen <neonguyen.ca@gmail.com>
2023-07-11 22:20:50 -0600

    20.1 Controlled Inputs Forms

--- src/views/AddNewBlog.js
+++ src/views/AddNewBlog.js
@@ -5,26 +5,27 @@ const AddNewBlog = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
 
-    const handleOnClickSubmit = () => {
+    const handleOnClickSubmit = (event) => {
+        event.preventDefault();
         console.log('>>>check data:', title, content)
     }
     return (
         <div className='add-new-container'>
-            <div className='text-add-new'>ADD NEW BLOG</div>
-            <div className='input-data'>
-                <label>Title:</label>
-                <input type='text' value={title}
-                    onChange={(e) => setTitle(e.target.value)} />
-            </div>
-            <div className='input-data'>
-                <label>Content:</label>
-                <input type='text' value={content}
-                    onChange={(e) => setContent(e.target.value)} />
-            </div>
-            <button className='btn'
-                onClick={handleOnClickSubmit}>
-                Submit
-            </button>
+            <form onSubmit={handleOnClickSubmit}>
+                <div className='text-add-new'>ADD NEW BLOG</div>
+                <div className='input-data'>
+                    <label>Title:</label>
+                    <input type='text' value={title}
+                        onChange={(e) => setTitle(e.target.value)} />
+                </div>
+                <div className='input-data'>
+                    <label>Content:</label>
+                    <input type='text' value={content}
+                        onChange={(e) => setContent(e.target.value)} />
+                </div>
+                {/* <button className='btn' onClick={handleOnClickSubmit}>Submit</button> */}
+                <button className='btn' type='submit'>Submit</button>
+            </form>
         </div>
     )
 }
